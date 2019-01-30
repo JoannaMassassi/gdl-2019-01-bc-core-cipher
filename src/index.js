445ac1bac37a37275310f480dfb59bb24534d4d0
JoannaMassassi <issassam95@hotmail.com>
2019-01-30 14:09:21 -0600

    Se agregaron pantallas HTML, botones y funciones

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+function encodeText() {
+	document.getElementById("pantalla_cifrar").style.display = "block";
+	document.getElementById("principal").innerHTML = "";
+
+}
+
+function decodeText() {
+	document.getElementById("pantalla_descifrar").style.display = "block";
+	document.getElementById("principal").innerHTML = "";	
+
+}
+
+function homePage() {
+	location.reload();
+}
