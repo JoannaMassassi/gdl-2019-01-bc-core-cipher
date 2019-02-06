816c97d683a4fdc5d762221474c6d7eeef99dbf1
JoannaMassassi <issassam95@hotmail.com>
2019-02-06 16:46:47 -0600

    Proyecto actualizado con funciones correctas, test realizados y pasados.

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,40 @@
-window.cipher = {
-  // ... 
+const ciCode = (offset, string) => {
+	let save = string.split("").map(letter => {
+	const numero = (letter.charCodeAt(0) - 65 + offset) % 26 + 65;
+	let result1 = String.fromCharCode(numero);
+
+	return result1;
+	});
+
+	let result2 = save.join("");
+	return result2;
 };
+
+const ciCode2 = (offset,string) => {
+	let save2 = string.split("").map(letter => {
+	const numero2 = (letter.charCodeAt(0) + 65 - offset) % 26 + 65;
+	let result1a = String.fromCharCode(numero2);
+
+	return result1a;
+	});
+
+	let result2a = save2.join("");
+	return result2a;
+};
+
+window.cipher = {
+	encode: ciCode,
+	decode: ciCode2,
+};
+	
+
+
+	
+
+
+
+	
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
 
-function encodeText() {
+function encodeWeb() {
 	document.getElementById("pantalla_cifrar").style.display = "block";
-	document.getElementById("principal").innerHTML = "";
+	document.getElementById("principal").innerHTML = "";  /* LANZA LA PANTALLA PARA CIFRAR */
 
 }
+	document.getElementById("encode").addEventListener("click", encodeWeb);
 
-function decodeText() {
-	document.getElementById("pantalla_descifrar").style.display = "block";
+function decodeWeb() {
+	document.getElementById("pantalla_descifrar").style.display = "block"; /*LANZA LA PANTALLA PARA DESCRIFRAR */
 	document.getElementById("principal").innerHTML = "";	
 
 }
+	document.getElementById("decode").addEventListener("click", decodeWeb);
 
-function homePage() {
-	location.reload();
+/*MANDA A LA PANTALLA DE INICIO1 */
+
+function homePage() { 
+	location.reload(); 
 }
+
+	document.getElementById("Home2").addEventListener("click", homePage);
+
+	document.getElementById("Home").addEventListener("click", homePage);
+
+	document.getElementById("cipher").addEventListener("click", encoding);
+
+	document.getElementById("decipher").addEventListener("click", decoding);
+
+//FUNCION CIFRAR EL MENSAJE!!!!
+function encoding() {
+	let keyNumber  = parseInt(document.getElementById("positions").value); //GUARDAR VALOR DEL OFFSET
+	let message = document.getElementById("campo_cifrar").value.toUpperCase(); // GUARDAR VALOR DEL INPUT MENSAJE EN MAYUSCULAS
+	
+	
+	let documentJs = window.cipher.encode(keyNumber, message);
+
+	document.getElementById('resultadoTot').innerHTML = documentJs;
+
+}
+
+
+
+    //FUNCION DESCIFRAR EL MENSAJE!!!!
+	function decoding() {
+		let keyNumber  = parseInt(document.getElementById("positions2").value); //GUARDAR VALOR DEL OFFSET
+		let message = document.getElementById("campo_descifrar").value.toUpperCase(); // GUARDAR VALOR DEL INPUT MENSAJE EN MAYUSCULAS
+	
+			let documentJs2 = window.cipher.decode(keyNumber, message);
+
+			document.getElementById('resultadoTot2').innerHTML = documentJs2;
+    
+
+}
+    
+	
+
